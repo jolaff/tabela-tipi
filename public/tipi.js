10c68c0de170e62d63aa5f3b6bdcc7b1d7861304
jolaff <joel.landgraf@gmail.com>
2018-10-02 05:48:42 -0300

    developing main functions

--- public/tipi.js
+++ public/tipi.js
@@ -1,21 +1,32 @@
-const resultado = document.querySelector(".grid-result");
+const resultado = document.querySelector('.grid-result');
 
-function renderNCM(doc) {
-  let div = document.createElement("div");
-  let seção = document.createElement("h4");
-  let descrição = document.createElement("span");
-  seção.innerText = doc.id;
-  descrição.innerText = doc.data().descrição;
-  div.setAttribute("class", "grid-seção");
-  div.appendChild(seção);
-  div.appendChild(descrição);
+function renderSeção(doc) {
+  let div = document.createElement('div');
+  //let seção = document.createElement('h4');
+  //let descrição = document.createElement('span');
+  div.innerText += `Seção ${doc.id}\n`;
+  div.innerText += doc.data().descrição;
+  div.setAttribute('class', 'grid-seção');
+  //div.appendChild(seção);
+  //div.appendChild(descrição);
   resultado.appendChild(div);
 }
 
-db.collection("seção")
+db.collection('seção')
   .get()
   .then(snapshot => {
     snapshot.docs.forEach(doc => {
-      renderNCM(doc);
+      renderSeção(doc);
     });
   });
+
+setTimeout(() => {
+  const principal = [].slice.call(
+    document.getElementsByClassName('grid-seção')
+  );
+  principal.forEach((el, index) => {
+    el.addEventListener('click', ev => {
+      console.log(ev.target);
+    });
+  });
+}, 3000);

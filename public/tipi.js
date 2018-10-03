d6609b5f530da2b2d3a1a4d97dfccc603422c18a
jolaff <joel.landgraf@gmail.com>
2018-10-03 17:11:15 -0300

    keep programming

--- public/tipi.js
+++ public/tipi.js
@@ -1,32 +1,21 @@
-const resultado = document.querySelector('.grid-result');
+const resultado = document.querySelector(".grid-result");
 
 function renderSeção(doc) {
-  let div = document.createElement('div');
+  let div = document.createElement("div");
   //let seção = document.createElement('h4');
   //let descrição = document.createElement('span');
   div.innerText += `Seção ${doc.id}\n${doc.data().descrição}`;
-  
-  div.setAttribute('class', 'grid-seção');
+  div.setAttribute("data-id", doc.id);
+  div.setAttribute("class", "grid-seção");
   //div.appendChild(seção);
   //div.appendChild(descrição);
   resultado.appendChild(div);
 }
 
-db.collection('seção')
+db.collection("seção")
   .get()
   .then(snapshot => {
     snapshot.docs.forEach(doc => {
       renderSeção(doc);
     });
   });
-
-setTimeout(() => {
-  const principal = [].slice.call(
-    document.getElementsByClassName('grid-seção')
-  );
-  principal.forEach((el, index) => {
-    el.addEventListener('click', ev => {
-      console.log(ev.target);
-    });
-  });
-}, 3000);

bfcb327d2851da1ee793444f0c91c43b8cdce5c0
jolaff <joel.landgraf@gmail.com>
2018-10-11 05:48:33 -0300

    starting to develop on node.js

--- tipi.js
+++ tipi.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const admin = require('firebase-admin');
 
+// Inicialização do SDK Firebase
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+const seção = db.collection('seção');
+const todos = seção.get().then(snapshot => {
+  snapshot.forEach(doc => {
+    console.log(doc.id, '|=>', doc.data());
+  });
+});
+
+// Inicialização do Express
 const app = express();
 
+// Configurando ejs como Template Engine
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Esta é a TIPI');
+  res.render('index');
 });
 
 app.listen(3000);
